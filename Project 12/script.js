56cad2a1404270c3f9c481568aaa766ff7dd7d09
Vishal Vashishth <vishal112vp@gmail.com>
2025-10-13 22:11:32 +0530

    wheather-app

--- Project 12/script.js
+++ Project 12/script.js
@@ -1,4 +1,4 @@
-const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxx";
+const apiKey = "xxxxx";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
