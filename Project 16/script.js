9097012bb3dc78588c2827af27dc3a9a045f2151
Vishal Vashishth <vishal112vp@gmail.com>
2026-02-19 10:27:52 +0530

    Hide and Show

---
+++ Project 16/script.js
@@ -0,0 +1,14 @@
+
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.addEventListener("click", function() {
+    if (password.type === "password") {
+        password.type = "text";
+        eyeicon.src = "./eye-open.png";
+    }
+    else {
+        password.type = "password";
+        eyeicon.src = "./eye-close.png";
+    }
+});
